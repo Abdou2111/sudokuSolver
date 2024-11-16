10ea2c7072b3455f4249b954964228db803f97cc
Abdou2111 <abdelghafour.rahmouni@gmail.com>
2024-11-16 14:24:34 -0500

    Major

--- GameSolver.java
+++ GameSolver.java
@@ -7,21 +7,21 @@
  * @since       1.0 (25 October 2024)
  */
 
-public interface GameSolver {
+
+ public interface GameSolver {
+
 
     /**
      * Solves the given game state.
      *
-     * @param game the current game state
-     * @return the solution, such as the next move or a winning strategy
+     * @return 
      */
     boolean solve();
 
+
     /**
      * Provides a description of the solution.
-     *
-     * @param solution the solution obtained from the solve method
-     * @return a human-readable explanation of the solution
      */
     void printSolution();
 }
+
